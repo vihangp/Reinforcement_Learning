fba04ba2eb8b0eb1e75755dc442df1d11b9015de
vihangp <vihangppatil@gmail.com>
2018-05-17 15:24:12 +0200

    Printing and saving according to worker 1 count

--- A3C_Distributed/worker_test.py
+++ A3C_Distributed/worker_test.py
@@ -11,16 +11,18 @@ class Worker():
         self.num_global_steps = steps
 
         self.local_network = PolicyValueNetwork(self.thread_name, self.task_id)
+        print("Initializing:", self.thread_name)
 
 
     def play(self,master_session, coord):
+        print("I am Survivor:",self.thread_name)
 
         while not coord.should_stop():
 
-            print(self.thread_name,": incrementing var")
-            print(global_step)
             _, global_step = master_session.run([self.global_network.var.assign_add(1.0), self.global_network.global_step.assign_add(1.0)])
             sleep(1.0)
+            print(self.thread_name, ": incrementing var")
+            print(global_step)
 
             if global_step > self.num_global_steps:
                 coord.request_stop()
