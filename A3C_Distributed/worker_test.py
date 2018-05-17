31c200fcd802731d420b67df28570f69eb9e6162
vihangp <vihangppatil@gmail.com>
2018-05-17 21:22:43 +0200

    Printing and saving according to worker 1 count

--- A3C_Distributed/worker_test.py
+++ A3C_Distributed/worker_test.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import tensorflow as tf
 from time import sleep
+import numpy as np
 
 class Worker():
     def __init__(self, master_name, thread_name, global_network):
@@ -12,7 +13,7 @@ class Worker():
     def play(self, local_session, master_session, coord):
 
         for i in range(5):
-            feed_dict = {self.global_network.a: [1]}
+            feed_dict = {self.global_network.a: np.array([1])}
             val, _ = master_session.run([self.global_network.c, self.global_network.b], feed_dict)
             sleep(1.0)
             print(self.thread_name, ": incrementing var current val", val)
