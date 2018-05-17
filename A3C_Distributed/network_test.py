0143dfb855522436a4587fc36976b5b513d08274
vihangp <vihangppatil@gmail.com>
2018-05-17 18:02:34 +0200

    Printing and saving according to worker 1 count

--- A3C_Distributed/network_test.py
+++ A3C_Distributed/network_test.py
@@ -14,8 +14,8 @@ class GlobalNetwork():
         with tf.device(tf.train.replica_device_setter( worker_device=worker_device, cluster=cluster)):
 
             self.global_step = tf.train.get_or_create_global_step()
-            self.a = tf.get_variable("a", [1], initializer=tf.constant_initializer(1))
-            self.b = tf.get_variable("b", [1], initializer=tf.constant_initializer(0))
+            self.a = tf.get_variable("a", [1], initializer=tf.constant_initializer(0))
+            self.b = tf.get_variable("b", [1], initializer=tf.constant_initializer(1))
             self.c = tf.assign_add(self.a, self.b)
 
 class PolicyValueNetwork():
