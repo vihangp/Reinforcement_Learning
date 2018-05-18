3576cdc06ee17013bcb275f2301c33e991570faf
vihangp <vihangppatil@gmail.com>
2018-05-18 14:20:27 +0200

    Printing and saving according to worker 1 count

--- A3C_Distributed/cluster_example.py
+++ A3C_Distributed/cluster_example.py
@@ -62,10 +62,10 @@ def worker(worker_n):
     global_network = GlobalNetwork(cluster, worker_n)
     num_cores = multiprocessing.cpu_count()
 
-    workers = []
-    for i in range(num_cores):
-        worker_object = Worker(worker_n, "worker_{}{}".format(FLAGS.task_index, i + 1), global_network)
-        workers.append(worker_object)
+    # workers = []
+    # for i in range(num_cores):
+    #     worker_object = Worker(worker_n, "worker_{}{}".format(FLAGS.task_index, i + 1), global_network)
+    #     workers.append(worker_object)
 
     # init_op = tf.global_variables_initializer()
     #
@@ -77,26 +77,30 @@ def worker(worker_n):
 
     #with super.managed_session(server.target) as master_session, master_session.as_default():
 
-    with tf.train.MonitoredTrainingSession(master=server.target,
-                                           is_chief=(worker_n == 0)) as master_session:
-        while not master_session.should_stop():
+    master_session = tf.train.MonitoredTrainingSession(master=server.target,
+                                           is_chief=(worker_n == 0))
 
-        #while not super.should_stop():
-            coord = tf.train.Coordinator()
+    # with tf.train.MonitoredTrainingSession(master=server.target,
+    #                                        is_chief=(worker_n == 0)) as master_session:
 
-            threads = []
-            i = 1
-            for worker in workers:
-                work = lambda worker=worker: worker.play(master_session, coord)
-                t = threading.Thread(name="worker_{}{}".format(FLAGS.task_index, i + 1), target=work)
-                i = i + 1
-                threads.append(t)
-                t.start()
+    while not master_session.should_stop():
 
-            coord.join(threads)
+    #while not super.should_stop():
+        #coord = tf.train.Coordinator()
 
-            var = master_session.run(global_network.a)
-            print(worker_n, "Value:", var)
+        # threads = []
+        # i = 1
+        # for worker in workers:
+        #     work = lambda worker=worker: worker.play(master_session, coord)
+        #     t = threading.Thread(name="worker_{}{}".format(FLAGS.task_index, i + 1), target=work)
+        #     i = i + 1
+        #     threads.append(t)
+        #     t.start()
+
+        #coord.join(threads)
+
+        var = master_session.run(global_network.a)
+        print(worker_n, "Value:", var)
 
     #super.stop()
 
