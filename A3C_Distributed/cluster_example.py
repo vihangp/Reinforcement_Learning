2a36e600551f08ba291e7c20a7a2691a37d179e6
vihangp <vihangppatil@gmail.com>
2018-05-18 14:00:09 +0200

    Printing and saving according to worker 1 count

--- A3C_Distributed/cluster_example.py
+++ A3C_Distributed/cluster_example.py
@@ -95,7 +95,7 @@ def worker(worker_n):
 
             coord.join(threads)
 
-            var, _ = master_session.run(global_network.a)
+            var = master_session.run(global_network.a)
             print(worker_n, "Value:", var)
 
     #super.stop()
