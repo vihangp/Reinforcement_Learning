9f2c3c44a8e7d79f5eb36a7bb209e9147bc0441f
vihangp <vihangppatil@gmail.com>
2018-05-20 12:56:33 +0200

    Printing and saving according to worker 1 count

--- A3C_Distributed/network_test.py
+++ A3C_Distributed/network_test.py
@@ -15,7 +15,7 @@ class GlobalNetwork():
                                                        cluster=cluster)):
 
             self.global_step = tf.train.get_or_create_global_step()
-            self.b = tf.placeholder(shape=[1], dtype=tf.float32)
+            self.b = tf.placeholder(dtype=tf.float32)
             self.a = tf.Variable([1.0], dtype=tf.float32)
             #self.assign_double = tf.assign(self.a, 1+ self.a)
             self.assign_double = tf.assign(self.a, self.b)
@@ -26,4 +26,4 @@ class PolicyValueNetwork():
 
         with tf.device("/job:worker/task:%d" % task_id):
             with tf.variable_scope(thread_name):
-                    self.local_var = tf.Variable(task_id, name='var')
+                    self.local_var = tf.Variable([task_id], name='var')
